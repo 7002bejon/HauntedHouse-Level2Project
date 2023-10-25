20f9d83e620b80991df8004880abbb5025d4eaf6
7002bejon <114094974+7002bejon@users.noreply.github.com>
2023-10-25 14:56:40 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function initialize() {
+    playerTotalKeys = 0;
+    playerUnusedKeys = 0;
+    playerUnlockedStairs1 = false;
+    playerUnlockedStairs2 = false;
+    playerUnlockedKitchen = false;
+    playerUnlockedFinalRoom = false;
+    playerDeath = false;
+}
