0f661cdb4ec2a8f1b6f3da683088245576d3dabc
7002bejon <114094974+7002bejon@users.noreply.github.com>
2023-10-27 15:29:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function initialize() {
+    playerTracker = null;
     playerTotalKeys = 0;
     playerUnusedKeys = 0;
     playerUnlockedStairs1 = false;
@@ -6,4 +7,33 @@ function initialize() {
     playerUnlockedKitchen = false;
     playerUnlockedFinalRoom = false;
     playerDeath = false;
+}
+
+function changePlayerTrackerLR() {
+    playerTracker == "Living Room";
+}
+
+function changePlayerTrackerSTR1() {
+    playerTracker == "Stairs1";
+}
+
+function keyCheck() {
+    stairs1 = document.getElementById("KeyCheckStairs1");
+    if (playerUnusedKeys == 0 & playerUnlockedStairs1 == false) {
+        stairs1.innerHTML = "The door ahead seems to be locked, maybe there is a key you can find?";     
+    } else if (playerUnusedKeys!= 0 & playerUnlockedStairs1 == false) {
+        stairs1.innerHTML = "The door ahead is locked, but you can unlock it with a key";
+    }
+}
+
+function showUnlockButton() {
+
+}
+
+function unlock() {
+        if (playerTracker == "Stairs1") {
+            playerUnlockedStairs1 == true;
+            playerUnusedKeys -= 1;
+
+    }
 }
